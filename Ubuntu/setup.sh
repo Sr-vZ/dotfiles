3f37c1d5123014ced045260f5fe4dbb309a54807
Sourav De <srvz39@gmail.com>
2024-01-19 02:51:38 +0530

    Update setup.sh

--- Ubuntu/setup.sh
+++ Ubuntu/setup.sh
@@ -58,12 +58,13 @@ wget https://raw.githubusercontent.com/mcarvalho1/Nerd-fonts-Downloader-Script/m
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl https://raw.githubusercontent.com/Sr-vZ/dotfiles/main/tmux/tmux.conf -o ~/.tmux.conf
 
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
+
 
 wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
+alias nvim=./nvim-linux64/bin/nvim
 
-
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 echo "System update and upgrade, installation, and configuration completed!"
