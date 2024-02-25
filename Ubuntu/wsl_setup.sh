ee1a25a192f05f75a8d843f28e17de31878ad4ad
Sourav De <srvz39@gmail.com>
2024-02-26 02:05:47 +0530

    Create wsl_setup.sh

---
+++ Ubuntu/wsl_setup.sh
@@ -0,0 +1,7 @@
+
+# Install Zsh
+sudo apt install zsh
+# Install oh-my-zsh via curl 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
